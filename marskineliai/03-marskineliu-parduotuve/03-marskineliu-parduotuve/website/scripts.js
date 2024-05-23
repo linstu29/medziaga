93359a0db00b545d6f465004033fbed59ab22547
linstu29 <lina.stundyte@gmail.com>
2024-05-23 19:42:59 +0300

    js projektas marskineliu parduotuve, uzdarant modal jis isvalomas, o atidarant idedam title

--- marskineliai/03-marskineliu-parduotuve/03-marskineliu-parduotuve/website/scripts.js
+++ marskineliai/03-marskineliu-parduotuve/03-marskineliu-parduotuve/website/scripts.js
@@ -8,7 +8,7 @@ if (document.querySelector('.home-page')) {
     fetch('http://localhost:7790/products')
         .then(response => response.json())
         .then(productsData => {
-            console.log(productsData);
+            // console.log(productsData);
 
             let block = document.querySelector('.products-list');
             // console.log(block);
@@ -22,11 +22,11 @@ if (document.querySelector('.home-page')) {
                     </div>
                     <h3>${product.title}</h3>
                     <p>
-                    ${ product.priceMin == product.priceMax
-                    ? product.priceMin
-                    : product.priceMin + ' - ' + product.priceMax } €
-                </p>
-                <button onclick="openModal(${product.id})">Peržiūra</button>
+                        ${ product.priceMin == product.priceMax
+                        ? product.priceMin
+                        : product.priceMin + ' - ' + product.priceMax } €
+                    </p>
+                    <button onclick="openModal(${product.id})">Peržiūra</button>
                 </div>`;
             }
         });
@@ -37,20 +37,36 @@ if (document.querySelector('.home-page')) {
 // ================================================
 
 if (document.querySelector('.home-page')) {
-   // console.log('esu pradzioje');
-   let modalWrapper = document.querySelector('.modal-wrapper');
+    // console.log('esu pradzioje');
+    let modalWrapper = document.querySelector('.modal-wrapper');
 
-   function openModal(productId) {
-    console.log('modal opened', productId);
-    modalWrapper.classList.add('active');
-}
-function closeModal() {
-    modalWrapper.classList.remove('active');
-    console.log('modal closed');
-    
-}
+    function openModal(productId) {
+        // console.log('modal opened', productId);
+        modalWrapper.classList.add('active');
 
-document.querySelector('.modal-wrapper .backdrop').addEventListener('click', closeModal);
+        fetch('http://localhost:7790/products/' + productId)
+            .then(response => response.json())
+            .then(productData => {
+                console.log(productData);
+
+                let modalContent = modalWrapper.querySelector('.modal');
+                // console.log(modalContent);
+
+                modalContent.innerHTML = `
+                    <h2>${productData.title}</h2>
+                `;
+            });
+    }
+
+    function closeModal() {
+        modalWrapper.classList.remove('active');
+        // console.log('modal closed');
+        modalWrapper.querySelector('.modal').innerHTML = `
+            <h2>Produkto informacija</h2>
+            <p>Kraunasi...</p>`;
+    }
+
+    document.querySelector('.modal-wrapper .backdrop').addEventListener('click', closeModal);
 }
 
 // ================================================
