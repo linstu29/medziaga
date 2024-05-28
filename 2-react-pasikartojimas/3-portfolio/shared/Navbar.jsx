87d75a734635d27feaf3529d0b9ad6b83bfd73e2
linstu29 <lina.stundyte@gmail.com>
2024-05-28 19:41:32 +0300

    react, portfolio projektas, sudetos nuorodos i navbar

--- 2-react-pasikartojimas/3-portfolio/shared/Navbar.jsx
+++ 2-react-pasikartojimas/3-portfolio/shared/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <nav>
+            <div className="container">
+                <div className="brand">
+                    {/* <a href="/">Vardenis Pavardenis portfolio</a> */}
+                    <NavLink to="/">Vardenis Pavardenis portfolio</NavLink>
+                </div>
+                <ul>
+                    <li><NavLink to='/'>Pradžia</NavLink></li>
+                    <li><NavLink to='/projektai'>Projektai</NavLink></li>
+                    <li><NavLink to='/cv'>CV</NavLink></li>
+                    <li><NavLink to='/susisiekti'>Susisiekti</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
--- 2-react-pasikartojimas/3-portfolio/website/src/App.jsx
+++ 2-react-pasikartojimas/3-portfolio/website/src/App.jsx
@@ -2,33 +2,31 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
-import ProjectsPage from './pages/ProjectsPage';
-import CVpage from './pages/CVpage';
+import CVPage from './pages/CVPage';
 import ContactUsPage from './pages/ContactUsPage';
+import ProjectsPage from './pages/ProjectsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-  
-
-  return (
-    <>
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<MainLayout />}>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/projektai' element={<ProjectsPage />} />
-                <Route path='/cv' element={<CVpage />} />
-                <Route path='/susisiekti' element={<ContactUsPage />} />
-                <Route path='*' element={<NotFoundPage />} />
-            </Route>
-            {/* <Route path='/sistema' element={<DashboardLayout />}>
-                <Route path='/' element={<DashboardPradzia/>} />
-                <Route path='/mokejimai' element={<DashboardPayments/>} />
-            </Route> */}
-        </Routes>
-    </BrowserRouter>
-</>
-  
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<MainLayout />}>
+                        <Route path='/' element={<HomePage />} />
+                        <Route path='/projektai' element={<ProjectsPage />} />
+                        <Route path='/cv' element={<CVPage />} />
+                        <Route path='/susisiekti' element={<ContactUsPage />} />
+                        <Route path='*' element={<NotFoundPage />} />
+                    </Route>
+                    {/* <Route path='/sistema' element={<DashboardLayout />}>
+                        <Route path='/' element={<DashboardPradzia/>} />
+                        <Route path='/mokejimai' element={<DashboardPayments/>} />
+                    </Route> */}
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
