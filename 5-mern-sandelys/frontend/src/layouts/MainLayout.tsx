4fd8adb47f66164be2ea2925ae9c0298bdb6ffef
linstu29 <lina.stundyte@gmail.com>
2024-06-17 18:57:20 +0300

    mern sandelys, react navigacija

--- 5-mern-sandelys/frontend/src/layouts/MainLayout.tsx
+++ 5-mern-sandelys/frontend/src/layouts/MainLayout.tsx
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
+import styles from './MainLayout.module.css'
+
 const MainLayout = () => {
     return (
-        <div>MainLayout
+        <div><nav className={styles.nav}>
+        <div>
+            <Link to='/'>Sandėlys</Link>
+        </div>
+        <ul>
+            <li><Link to='/'>Pradžia</Link></li>
+            <li><Link to='/sukurti'>Sukurti naują</Link></li>
+        </ul>
+    </nav>
             <Outlet />
         </div>
 
